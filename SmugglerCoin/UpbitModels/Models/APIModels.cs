891bf3e3fde78b4a407b5b8779b66545040e6f92
pjh2104 <ParkJungwhan@users.noreply.github.com>
2025-10-21 12:19:01 +0900

    조회 결과 모델 생성중....

--- SmugglerCoin/UpbitModels/Models/APIModels.cs
+++ SmugglerCoin/UpbitModels/Models/APIModels.cs
@@ -22,8 +22,75 @@ public class BaseCandle
     public int unit { get; set; }
 }
 
-public class MinutesCandle : BaseCandle
+public class CandleSeconds : BaseCandle
 {
-    public int opening_timestamp { get; set; }
-    public int closing_timestamp { get; set; }
+}
+
+public class CandleMinutes : BaseCandle
+{
+}
+
+public class CandleDays : BaseCandle
+{
+    public decimal prev_closing_price { get; set; }
+    public decimal change_price { get; set; }
+    public string change_rate { get; set; } = string.Empty;
+    public string change { get; set; } = string.Empty;
+}
+
+public class CandleWeeks : BaseCandle
+{
+}
+
+public class CandleMonths : BaseCandle
+{
+}
+
+public class CandleYears : BaseCandle
+{
+}
+
+public class TradeTicks
+{
+    public DateTime trade_date_utc { get; set; }
+    public DateTime trade_time_utc { get; set; }
+    public long timestamp { get; set; }
+    public decimal trade_price { get; set; }
+    public decimal trade_volume { get; set; }
+    public decimal prev_closing_price { get; set; }
+    public decimal change_price { get; set; }
+    public string change_rate { get; set; } = string.Empty;
+    public string change { get; set; } = string.Empty;
+    public long sequential_id { get; set; }
+    public string ask_bid { get; set; }
+}
+
+public class PairCurrentTicks   // 페어 단위 현대가 조회
+{
+    public string market { get; set; }
+    public string trade_date { get; set; }
+    public string trade_time { get; set; }
+    public string trade_date_kst { get; set; }
+    public string trade_time_kst { get; set; }
+    public float trade_timestamp { get; set; }
+    public double opening_price { get; set; }
+    public double high_price { get; set; }
+    public double low_price { get; set; }
+    public double trade_price { get; set; }
+    public double prev_closing_price { get; set; }
+    public string change { get; set; }   //EVEN, RISE, FALL
+    public string change_price { get; set; }
+    public double change_rate { get; set; }
+    public double signed_change_price { get; set; }
+    public double signed_change_rate { get; set; }
+    public double trade_volume { get; set; }
+    public double acc_trade_price { get; set; }
+    public double acc_trade_price_24h { get; set; }
+    public double acc_trade_volume { get; set; }
+    public double acc_trade_volume_24h { get; set; }
+    public double highest_52_week_price { get; set; }
+    public string highest_52_week_date { get; set; } //yyyy-MM-dd
+    public double lowest_52_week_price { get; set; }
+    public string lowest_52_week_date { get; set; }  //yyyy-MM-dd
+    public float timestamp { get; set; }
 }
